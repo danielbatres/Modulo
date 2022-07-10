37df85a47129a144c1799cde8b40b05caad6bc00
danielbatres <danielbatresuser@gmail.com>
2022-07-10 08:31:18 -0600

    Indicaciones

---
+++ SQL/Actividades/Actividad-final/ActividadFinal.sql
@@ -0,0 +1,18 @@
+/*
+
+	Actividad Final de Bases de Datos
+
+	Daniel Alessandro Garcia Batres #13
+
+	Indicaciones: Crear una consulta con todo el proceso para la generación de Administración de una
+	Bases de Datos, Consultas Simples, Consultas con Funciones y Subconsultas.
+
+*/
+
+/*
+
+	1. Considere el esquema relacional BIBLIOTECA que sirve para llevar el control de libros,
+	   lectores y préstamos de libros. Realice las siguientes consultas sobre los datos,
+	   ocupando funciones de agregación:
+
+*/
