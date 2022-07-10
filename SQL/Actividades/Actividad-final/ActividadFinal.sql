523e6a9f2ca82662889e7aa217f923a7e2bc696b
danielbatres <danielbatresuser@gmail.com>
2022-07-10 09:53:08 -0600

    Ejercicios

--- SQL/Actividades/Actividad-final/ActividadFinal.sql
+++ SQL/Actividades/Actividad-final/ActividadFinal.sql
@@ -77,7 +77,11 @@ select l.Titulo as "Nombre libro", e.NombreEd as "Editorial" from Libros l
 inner join Editoriales e on e.CodEditorial = l.CodEditorial
 order by Titulo, NombreEd desc;
 
--- j) Obtenga una lista de los libros con el número total de copias existentes de cada uno.select Titulo, sum(NumCopias) as "Copias" from Libros l 
-inner join CopiasLibros c on l.IdLibro = c.IdLibro group by Titulo-- k) Muestre por cada sucursal el numero total de copias que posee y ordenelas de
+-- j) Obtenga una lista de los libros con el número total de copias existentes de cada uno. 
+
+select Titulo, sum(NumCopias) as "Copias" from Libros l 
+inner join CopiasLibros c on l.IdLibro = c.IdLibro group by Titulo
+
+-- k) Muestre por cada sucursal el numero total de copias que posee y ordenelas de
 -- menor a mayor.
 
